7289ff3cc6f975ecae7d697ce380b5537d90311e
siso <siso25@zoho.com>
2020-12-13 23:23:50 +0900

    コマンドラインのオプションの処理を追加
    コマンドラインのオプションを受け取り、年と月にセットする処理を追加。
    また、自然数かどうか、月の範囲内に収まっているかどうかのチェックを追加。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,35 +1,66 @@
+#!/usr/bin/env ruby
 require 'date'
+require 'optparse'
 
-# 年と月を表示
-year = Date.today.year
-month = Date.today.month
+def is_positive_integer(string)
+  if !string.nil? && string.match?(/\d+/) && string.to_i > 0
+    return true
+  end
+
+  false
+end
+
+def is_within_range_of_month(str_month)
+  month = str_month.to_i
+  january = 1
+  december = 12
+  if january <= month && month <= december
+    return true
+  end
+  false
+end
+
+def calculate_start_position(first_date)
+  width_of_day = 2
+  right_blank = 1
+  start_position = (width_of_day + right_blank) * first_date.wday
+end
+
+
+# 年と月の表示
+options = ARGV.getopts("y:", "m:")
+option_year = options["y"]
+option_month = options["m"]
+
+if is_positive_integer(option_year) 
+  year = option_year.to_i
+else
+  year = Date.today.year
+end
+
+if is_positive_integer(option_month) && is_within_range_of_month(option_month)
+  month = option_month.to_i
+else
+  month = Date.today.month
+end
+
 puts "#{month.to_s}月 #{year.to_s}".center(20)
 
 # 曜日を表示
 days_of_week = %w(日 月 火 水 木 金 土)
 puts days_of_week.join(" ")
 
-# 月の最終日を取得する
+# 日付部分の表示
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
-# 開始位置を決める
-width_of_day = 2
-right_blank = 1
-start_position = (width_of_day + right_blank) * first_date.wday
-
-# 日付部分の表示
-# 1日〜最終日までループ
 (first_date..last_date).each do |date|
-  # 右詰で出力
+  blank = " "
   if date.day == 1
-    print date.day.to_s.rjust(start_position + width_of_day)
-  else
-    print date.day.to_s.rjust(width_of_day)
+    print blank * calculate_start_position(date)
   end
-  print " " * right_blank
+  print date.day.to_s.rjust(2) + blank
 
-  # 土曜日だったら改行
   saturday = 6
   if date.wday == saturday
     print "\n"
