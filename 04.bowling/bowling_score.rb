e66c0e487383ef4a7df6808a62306f8f4b29e07d
siso <siso25@zoho.com>
2020-12-28 15:22:55 +0900

    rubocopの警告項目を修正
    各メソッドのリファクタリング。
    strike?メソッドのテストを追加。
    テストが全てパスし、rubocopの警告が0になったことを確認。

--- 04.bowling/bowling_score.rb
+++ 04.bowling/bowling_score.rb
@@ -1,16 +1,19 @@
-def shotted_all_pins?(pins)
-  if pins.is_a?(Integer)
-    pins == 10
-  else
-    pins == 'X'
-  end
+#! /usr/bin/env ruby
+
+# frozen_string_literal: true
+
+def shotted_all_pins?(shotted_pins)
+  all_pins = shotted_pins.is_a?(Integer) ? 10 : 'X'
+  shotted_pins == all_pins
 end
 
 def strike?(frame)
-  shotted_all_pins?(frame.first)
+  shotted_all_pins?(frame[0])
 end
 
 def spare?(frame)
+  return false if frame[0] == 10
+
   frame_score = frame.inject { |sum, shot| sum + shot }
   frame_score == 10
 end
@@ -22,7 +25,7 @@ def calculate_strike_score(frames, frame_number)
 
   current_frame_score = 10
   next_frame_score = next_frame[0]
-  after_next_frame_score = 
+  after_next_frame_score =
     if strike?(next_frame) && frame_number < 9
       after_next_frame[0]
     else
@@ -34,8 +37,10 @@ end
 
 def calculate_spare_score(frames, frame_number)
   current_index = frame_number - 1
+  next_frame = frames[current_index + 1]
+
   current_frame_score = 10
-  next_frame_score = frames[current_index + 1][0]
+  next_frame_score = next_frame[0]
 
   current_frame_score + next_frame_score
 end
@@ -46,20 +51,14 @@ def separate_by_frame(shots)
   frames = []
   frame = []
   shots.each_with_index do |shot, idx|
-    if shotted_all_pins?(shot) && frame.empty? && frame_number < 10
-      frame << shot << 0
-    else
-      frame << shot
-    end
+    frame << shot
+    frame << 0 if strike?(frame) && frame_number < 10
 
-    if (frame_number < 10 && frame.size == 2) ||
-      (idx == last_index && frame.size == 2) ||
-      (frame_number == 10 && frame.size == 3)
+    next if frame.size < 2 || (frame_number == 10 && idx != last_index)
 
-      frames << frame
-      frame_number += 1
-      frame = []
-    end
+    frames << frame
+    frame_number += 1
+    frame = []
   end
 
   frames
@@ -71,7 +70,7 @@ shots = scores.map { |s| shotted_all_pins?(s) ? 10 : s.to_i }
 frames = separate_by_frame(shots)
 
 point = 0
-frames.each.with_index(frame_number = 1) do |frame, frame_number|
+frames.each.with_index(1) do |frame, frame_number|
   point +=
     if strike?(frame) && frame_number < 10
       calculate_strike_score(frames, frame_number)
